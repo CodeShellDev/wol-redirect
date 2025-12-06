3c878c9e09c8acb405a3e57c18c15f8b9be9bded
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-12-06 18:08:49 +0100

    fix

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,16 +1,12 @@
 const fs = require("fs")
-const path = require("path")
 
 function exists(p) {
-	fs.stat(p, function (err, stat) {
-		if (err == null) {
-			return true
-		} else if (err.code === "ENOENT") {
-			return false
-		} else {
-			return false
-		}
-	})
+	try {
+		fs.statSync(p)
+		return true
+	} catch (err) {
+		return false
+	}
 }
 
 module.exports = { exists }
