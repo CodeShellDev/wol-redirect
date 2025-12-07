f7530e41b1677d206d532749b9869cda66a8ce65
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-12-07 10:50:12 +0100

    fix urlparse

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 		return res.status(200).end()
 	}
 
-	url = new URL(req.url)
+	url = URL.parse(req.url, `${req.protocol}://${req.hostname}`)
 
 	log.logger.info(`${req.method} ${url.pathname} ${url.search}`)
 	next()
